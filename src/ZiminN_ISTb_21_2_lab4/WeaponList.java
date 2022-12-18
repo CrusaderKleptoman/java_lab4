de007c7895f3dbc456c6822b6ef9c87fe26e53f4
CrusaderKleptoman <64123771+CrusaderKleptoman@users.noreply.github.com>
2022-12-19 06:25:20 +0800

    Список оружия
    Реализовано добавление/удаление оружие и вывод списков

--- src/ZiminN_ISTb_21_2_lab4/WeaponList.java
+++ src/ZiminN_ISTb_21_2_lab4/WeaponList.java
@@ -3,6 +3,36 @@ package ZiminN_ISTb_21_2_lab4;
 import java.util.ArrayList;
 
 public class WeaponList {
-    ArrayList<MeleeWeapon> meleeWeaponList = new ArrayList<>();
-    ArrayList<RangeWeapon> rangeWeaponList = new ArrayList<>();
+    private ArrayList<MeleeWeapon> meleeWeaponList = new ArrayList<>();
+    private ArrayList<RangeWeapon> rangeWeaponList = new ArrayList<>();
+
+    public void addMeleeWeapon(MeleeWeapon meleeWeapon) {
+        meleeWeaponList.add(meleeWeapon);
+    }
+    public void addRangeWeapon(RangeWeapon rangeWeapon) {
+        rangeWeaponList.add(rangeWeapon);
+    }
+    public void removeMeleeWeapon(MeleeWeapon meleeWeapon) {
+        meleeWeaponList.remove(meleeWeapon);
+    }
+    public void removeRangeWeapon(RangeWeapon rangeWeapon) {
+        meleeWeaponList.remove(rangeWeapon);
+    }
+
+    public void writeMeleeWeaponList()
+    {
+        for (MeleeWeapon weapon: meleeWeaponList)
+        {
+            System.out.printf("Название оружия %s, урон оружия %d%s, модификатор попадания и урона %d, дистанция удара %d \n", weapon.getWeaponName(), weapon.getDamageDiceAmount(), weapon.getDamageDice().toString(), weapon.getWeaponSharpening(), weapon.getAttackRange());
+        }
+    }
+
+    public void writeRangeWeaponList()
+    {
+        for (RangeWeapon weapon: rangeWeaponList)
+        {
+            System.out.printf("Название оружия %s, урон оружия %d%s, модификатор попадания и урона %d, дистанция выстрела %d, запас боеприпасов %d \n", weapon.getWeaponName(), weapon.getDamageDiceAmount(), weapon.getDamageDice().toString(), weapon.getWeaponSharpening(), weapon.getAttackRange(), weapon.getAmmunition());
+        }
+        
+    }
 }
