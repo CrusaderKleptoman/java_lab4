fb47ee00aaba57121e6cfaa4f4ab6539baefe29f
CrusaderKleptoman <64123771+CrusaderKleptoman@users.noreply.github.com>
2022-12-19 05:33:05 +0800

    Боеприпасы
    Добавлено поле "боеприпасы" для оружия дальнего боя

--- src/ZiminN_ISTb_21_2_lab4/RangeWeapon.java
+++ src/ZiminN_ISTb_21_2_lab4/RangeWeapon.java
@@ -3,27 +3,45 @@ package ZiminN_ISTb_21_2_lab4;
 import java.util.Scanner;
 
 public class RangeWeapon extends BaseWeapon{
+    private int ammunition;
     public RangeWeapon()
     {
         super();
         this.setWeaponName("Камень");
         this.setAttackRange(20);
+        this.ammunition = 20;
     }
 
     public RangeWeapon(String weaponName, Dice damageDice, int damageDiceAmount, int accuracy, int attackRange)
     {
         super(weaponName, damageDice, damageDiceAmount, accuracy, attackRange);
+        this.ammunition = 20;
+    }
+
+    public RangeWeapon(String weaponName, Dice damageDice, int damageDiceAmount, int accuracy, int attackRange, int ammunition)
+    {
+        super(weaponName, damageDice, damageDiceAmount, accuracy, attackRange);
+        this.ammunition = ammunition;
     }
 
     public RangeWeapon(RangeWeapon rangeWeapon)
     {
         super(rangeWeapon);
+        this.ammunition = rangeWeapon.ammunition;
     }
+
+    public int getAmmunition() {return ammunition;}
+    public void setAmmunition(int ammunition) {this.ammunition = ammunition;}
 
     @Override
     public int Attack(int enemyArmour, int distance) {
         Scanner scanner = new Scanner(System.in);
         Boolean meleeShot = false;
+        if (ammunition == 0)
+        {
+            System.out.println("Отсутствуют боеприпасы для выстрела, завершение выстрела");
+            return 0;
+        }
         if (distance > this.getAttackRange())
         {
             System.out.println("Враг слишком далеко для выстрела, невозможно совершение атаки");
@@ -65,6 +83,7 @@ public class RangeWeapon extends BaseWeapon{
         }
 
         System.out.printf("Нанесено урона %d\n", rollDice);
+        ammunition--;
         return rollDice;
     }
 }
